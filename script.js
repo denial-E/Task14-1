0f8f22acd170538f46ee9998c5768a8b1a0f7e51
denial-E <shempeter221992@gmail.com>
2024-02-20 01:11:39 +0530

    run bro

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dataContainer = document.getElementById('data-container');
   
-    // Fetch data function
+    
     function fetchData(url) {
       return new Promise((resolve, reject) => {
         fetch(url)
@@ -19,14 +19,13 @@ document.addEventListener('DOMContentLoaded', () => {
    
     const apiUrl = 'https://bible-api.com/john%203:16';
   
-    // Fetch data and update UI
     fetchData(apiUrl)
       .then(data => {
-        // Update UI with fetched data
+        
         dataContainer.textContent = JSON.stringify(data);
       })
       .catch(error => {
-        // Handle errors
+       
         console.error('Error fetching data:', error);
       });
   });
